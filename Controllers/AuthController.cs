1e5d0177ad9c42796715e3226d27e2ba1f0dc105
Lomeshwar Sahu <lomeshwarsahu1996@gmail.com>
2026-02-18 16:29:45 +0530

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,111 @@
+﻿using EMISAPIS.DTOS;
+using Microsoft.Data.SqlClient;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMISAPIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly string _connectionString;
+        public AuthController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        // ✅ GET ALL
+        [HttpGet]
+        public async Task<IActionResult> GetStudents()
+        {
+            var users = new List<UserDTO>();
+
+            using SqlConnection con = new SqlConnection(_connectionString);
+            await con.OpenAsync();
+
+            using SqlCommand cmd = new SqlCommand("SELECT * FROM Users", con);
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+
+                users.Add(new UserDTO
+                {
+                    //user_id = Convert.ToInt32(reader["user_id"]),
+                    user_id = reader["user_id"] != DBNull.Value
+                ? Convert.ToInt32(reader["user_id"]) : 0,
+
+                    user_name = reader["user_name"].ToString(),
+                    e_mail_id = reader["e_mail_id"].ToString(),
+                    password = reader["password"].ToString(),
+                    user_type = reader["user_type"].ToString(),
+                    designation = reader["designation"].ToString(),
+                    address = reader["address"].ToString(),
+                    location_id = reader["location_id"] != DBNull.Value
+                ? Convert.ToInt32(reader["location_id"]) : 0,
+                    //location_id = Convert.ToInt32(reader["location_id"]),
+        //            pmis = reader["pmis"] != DBNull.Value
+        //? Convert.ToChar(reader["pmis"])
+        //: '\0',
+
+        //            hrms = reader["hrms"] != DBNull.Value
+        //? Convert.ToChar(reader["hrms"])
+        //: '\0',
+
+
+                    //public int user_id { get; set; }
+                    //public string? user_name { get; set; }
+                    //public string? e_mail_id { get; set; }
+                    //public string? password { get; set; }
+                    //public string? user_type { get; set; }
+                    //public string? designation { get; set; }
+                    //public string? address { get; set; }
+                    //public int? location_id { get; set; }
+                    //public char pmis { get; set; }
+                    //public char hrms { get; set; }
+                    //public char ems { get; set; }
+                    //public int? supplier_id { get; set; }
+                    //public int? empid { get; set; }
+
+
+                });
+            }
+
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudent(int id)
+        {
+            using SqlConnection con = new SqlConnection(_connectionString);
+            await con.OpenAsync();
+
+            string query = "SELECT * FROM Users WHERE user_id=@user_id";
+            using SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@user_id", id);
+
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            if (await reader.ReadAsync())
+            {
+                var users = new UserDTO
+                {
+                    user_id = reader["user_id"] != DBNull.Value
+                ? Convert.ToInt32(reader["user_id"]) : 0,
+
+                    user_name = reader["user_name"].ToString(),
+                    e_mail_id = reader["e_mail_id"].ToString(),
+                    password = reader["password"].ToString(),
+                    user_type = reader["user_type"].ToString(),
+                    designation = reader["designation"].ToString(),
+                    address = reader["address"].ToString(),
+                    location_id = reader["location_id"] != DBNull.Value
+                ? Convert.ToInt32(reader["location_id"]) : 0,
+                };
+
+                return Ok(users);
+            }
+
+            return NotFound("Student not found");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,40 @@
+﻿namespace EMISAPIS.Models
+{
+    public class User
+    {
+        public int user_id { get; set; }
+        public string? user_name { get; set; }
+        public string? e_mail_id { get; set; }
+        public string? password { get; set; }
+        public string? user_type { get; set; }
+        public string? designation { get; set; }
+        public string? address { get; set; }
+        public int? location_id { get; set; }
+        public char pmis { get; set; }
+        public char hrms { get; set; }
+        public char ems { get; set; }
+        public int? supplier_id { get; set; }
+        public int? empid { get; set; }
+        public char? TRIAL718 { get; set; }
+        public string? passcommon { get; set; }
+        public int? authority { get; set; }
+        public string? emailID { get; set; }
+        public string? HODName { get; set; }
+        public string? HODNo { get; set; }
+        public string? StoreName { get; set; }
+        public string? StoreNo { get; set; }
+        public int? facility_aut_id { get; set; }
+        public string? address2 { get; set; }
+        public string? storeOfficer { get; set; }
+        public string? storeOfficerMob { get; set; }
+        public DateTime? updateDT { get; set; }
+        public string? storelandline { get; set; }
+        public DateTime? lastPwdChangeDate { get; set; }
+        public string? flagPwdChange { get; set; }
+        public int? pwdChangeOTP { get; set; }
+        public int? DP_DistrictID_DME { get; set; }
+        public int? roleid { get; set; }
+        public string? IsCGMSCUser { get; set; }
+        public string OTP { get; set; }
+    }
+}
