95f47fb80a1ee040920bf188cc8528797da87f77
Lomeshwar Sahu <lomeshwarsahu1996@gmail.com>
2026-03-02 16:57:46 +0530

    all controls added

---
+++ DTOS/UpdateFileNoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EMISAPIS.DTOS
+{
+    public class UpdateFileNoDto
+    {
+        public int PoId { get; set; }
+        public string FileNo { get; set; }
+        public DateTime FileDate { get; set; }
+    }
+}
